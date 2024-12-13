217f217f1fc5ff86aeab01b656a0bbfe5ae648e2
YousefHishamGit <youseefhisham9@gmail.com>
2024-12-13 22:53:55 +0200

    Upload Front-end file with all we need

--- front-end/react-app/src/App.test.js
+++ front-end/react-app/src/App.test.js
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { test, expect } from '@jest/globals';
 import App from './App';
 
-test('renders the home page header', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const headerElement = screen.getByText(/Welcome to Our Shop/i);
-  expect(headerElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
-
---
+++ front-end/react-app/src/Components/Product.js
@@ -0,0 +1,17 @@
+import './ProductCss.css';
+import React from 'react';
+
+
+
+
+function product(){
+    return(
+        <>
+        <div className='product'>
+            <h1>Product</h1>
+        </div>
+        </>
+    );
+}
+
+export default product;
