2a215946938e127da0d0e8e4301969c68d8613b6
Kareem Abdullah <10812022100918@stud.cu.edu.eg>
2024-12-13 23:53:05 +0200

    Add GET request that get all products

--- back-end/server.js
+++ back-end/server.js
@@ -54,6 +54,15 @@ app.get("/products/:category?", async (req, res) => {
   }
 });
 
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.put("/updateproduct/:id", async (req, res) => {
   try {
     if (isNaN(req.body.price) || req.body.price <= 0) {
